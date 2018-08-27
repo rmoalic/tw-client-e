f53eb470346faa4395db84929cf4c8046afaa30f
Robin MOALIC <robin.moalic@yahoo.fr>
2018-08-27 16:30:49 +0200

    Authentifiation: changer la variable online pour utiliser un pin au lieu de la callback

--- app.py
+++ app.py
@@ -1,31 +1,80 @@
 import twitter
 import json
 import configparser
-from flask import Flask, render_template, request, redirect, url_for, flash, Markup
+from functools import wraps
+from requests_oauthlib import OAuth1Session
+from flask import Flask, render_template, request, redirect, url_for, flash, Markup, session
 app = Flask(__name__)
+
+online = True
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 app.secret_key = config["flask"].get('secret_key')
 
-api = twitter.Api(consumer_key=config["twitter"].get("consumer_key"),
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if session.get('oauth_token', '') == '':
+            return redirect(url_for('login_page'))
+        return f(*args, **kwargs)
+    return decorated
+
+def make_api():
+    return twitter.Api(consumer_key=config["twitter"].get("consumer_key"),
                   consumer_secret=config["twitter"].get("consumer_secret"),
-                  access_token_key=config["twitter"].get("access_token_key"),
-                  access_token_secret=config["twitter"].get("access_token_secret"),
+                  access_token_key=session['oauth_token'],
+                  access_token_secret=session['oauth_token_secret'],
                   cache=None,
                   sleep_on_rate_limit=True,
                   tweet_mode="extended")
 
-print(api.VerifyCredentials())
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    consumer_key=config["twitter"].get("consumer_key")
+    consumer_secret=config["twitter"].get("consumer_secret")
+    verifier = request.args.get("oauth_verifier", "")
+    if request.method == "GET" and verifier == "":
+        callback = url_for("login_page", _external=True) if online else "oob"
+        print (callback)
+        oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret,
+                                     callback_uri=callback)
+        resp = oauth_client.fetch_request_token("https://api.twitter.com/oauth/request_token")
+        url = oauth_client.authorization_url("https://api.twitter.com/oauth/authorize")
+        session['temp_oauth_token'] = resp.get('oauth_token')
+        session['temp_oauth_token_secret'] = resp.get('oauth_token_secret')
+        return render_template('login.html', url=url) if not online else redirect(url)
+    else:
+        if verifier != "":
+            pincode = verifier
+        else:
+            pincode = request.form['pin']
+        oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret,
+                                     resource_owner_key=session['temp_oauth_token'],
+                                     resource_owner_secret=session['temp_oauth_token_secret'],
+                                     verifier=pincode)
+        del session['temp_oauth_token']
+        del session['temp_oauth_token_secret']
+        resp = oauth_client.fetch_access_token("https://api.twitter.com/oauth/access_token")
+        session['oauth_token'] = resp.get('oauth_token')
+        session['oauth_token_secret'] = resp.get('oauth_token_secret')
+        return redirect('home')
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect('home')
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 @app.route("/timeline")
+@requires_auth
 def timeline():
     try:
-        tl = api.GetHomeTimeline()
+        tl = make_api().GetHomeTimeline()
         for t in tl:
             t = htmlize_tweet(t)
     except twitter.error.TwitterError as e:
@@ -34,12 +83,13 @@ def timeline():
     return render_template("tweet_list.html", tweets=tl)
 
 @app.route("/user")
+@requires_auth
 def user_summary():
     _id = request.args.get("id", None, type=int)
     try:
-        user = api.GetUser(user_id=_id)
+        user = make_api().GetUser(user_id=_id)
         user.profile_image_url_https = user.profile_image_url_https.replace('normal', '200x200')
-        tl = api.GetUserTimeline(user_id=_id)
+        tl = make_api().GetUserTimeline(user_id=_id)
         for t in tl:
             t = htmlize_tweet(t)
     except twitter.error.TwitterError as e:
@@ -49,10 +99,11 @@ def user_summary():
 
 
 @app.route("/list/timeline")
+@requires_auth
 def list_timeline():
     _id = request.args.get("id", type=int)
     try:
-        tl = api.GetListTimeline(list_id=_id)
+        tl = make_api().GetListTimeline(list_id=_id)
         for t in tl:
             t = htmlize_tweet(t)
     except twitter.error.TwitterError as e:
@@ -82,14 +133,17 @@ def htmlize_tweet(t):
     return t
 
 @app.route("/lists")
+@requires_auth
 def lists():
-    return render_template("lists.html", lists=api.GetLists())
+    print(session)
+    return render_template("lists.html", lists=make_api().GetLists())
 
 @app.route("/list/members")
+@requires_auth
 def list_members():
     _id = request.args.get("id", type=int)
     try:
-        members = api.GetListMembers(list_id=_id, skip_status=False)
+        members = make_api().GetListMembers(list_id=_id, skip_status=False)
         members = sort_userlist(members)
         for m in members:
             m.profile_image_url_https = m.profile_image_url_https.replace('normal', '200x200')
@@ -99,12 +153,14 @@ def list_members():
     return render_userlist(members)
 
 @app.route("/followers")
+@requires_auth
 def list_followers():
-    return list_generic(request, api.GetFollowers)
+    return list_generic(request, make_api().GetFollowers)
 
 @app.route("/following")
+@requires_auth
 def list_following():
-    return list_generic(request, api.GetFriends)
+    return list_generic(request, make_api().GetFriends)
 
 def list_generic(request, endpoint):
     _id = request.args.get("id", None, type=int)
@@ -124,14 +180,4 @@ def render_userlist(user_list):
 def sort_userlist(user_list):
     return sorted(user_list, key=lambda x: x.status.created_at_in_seconds if x.status else 0, reverse=True)
 
-#tl = api.GetHomeTimeline()
-#for status in tl:
-#    print("{}:\t{}\n\n".format(status.user.name, status.full_text))
-"""
-print(api.GetLists())
-
-tl = api.GetListTimeline(list_id=1006990616050454529)
-for status in tl:
-    print("{}: {}".format(status.user.name, status.full_text))
-"""
 app.run(debug=True, use_debugger=True, use_reloader=True)
